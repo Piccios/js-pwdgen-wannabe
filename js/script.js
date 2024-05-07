d6caa70435b23c640faa78893636745d383d70d6
Piccio <lorenzo.hl.picchi@gmail.com>
2024-05-07 13:15:24 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const userName = prompt("Qual e' il tuo nome?");
+
+const userSurname = prompt("Qual'e' il tuo cognome?");
+
+const userColor = prompt("Qual e' il tuo colore preferito?");
+
+let password = userName + userSurname + userColor + 24;
+
+console.log(password)
+
